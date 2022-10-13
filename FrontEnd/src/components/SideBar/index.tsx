101f840b69326aa7bb9f65f7a12869b98472ff25
wesley_wisch <wesley_wisch@hotmail.com>
2022-10-13 09:55:08 -0300

    feat: Completing the sidebar component.

--- FrontEnd/src/components/SideBar/index.tsx
+++ FrontEnd/src/components/SideBar/index.tsx
@@ -1,9 +1,91 @@
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School
+} from '@material-ui/icons';
+
+import person from '../../assets/person/2.jpeg';
+
 import { SideBarContainer } from './styles';
 
+type SidebarItemsProps = {
+  icon: React.ReactNode,
+  title: string;
+}
+
 export function SideBar() {
+  const sidebarItems = [
+    {
+      icon: <RssFeed className='sidebarIcon' />,
+      title: 'Feed',
+    },
+    {
+      icon: <Chat className='sidebarIcon' />,
+      title: 'Chats',
+    },
+    {
+      icon: <PlayCircleFilledOutlined className='sidebarIcon' />,
+      title: 'Vídeos',
+    },
+    {
+      icon: <Group className='sidebarIcon' />,
+      title: 'Groups',
+    },
+    {
+      icon: <Bookmark className='sidebarIcon' />,
+      title: 'Bookmarks',
+    },
+    {
+      icon: <HelpOutline className='sidebarIcon' />,
+      title: 'Questions',
+    },
+    {
+      icon: <WorkOutline className='sidebarIcon' />,
+      title: 'Jobs',
+    },
+    {
+      icon: <Event className='sidebarIcon' />,
+      title: 'Events',
+    },
+    {
+      icon: <School className='sidebarIcon' />,
+      title: 'Courses',
+    },
+  ] as SidebarItemsProps[];
+
   return (
     <SideBarContainer>
-      Sidebar
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          {sidebarItems.map((item, key) => (
+            <li key={key} className="sidebarListItem">
+              {item.icon}
+              <span className="sidebarListItemText">{item.title}</span>
+            </li>
+          ))}
+        </ul>
+
+        <button className="sidebarButton">
+          Show more
+        </button>
+
+        <hr className='sidebarHr' />
+
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img src={person} alt="Jane doe" className="sidebarFriendImg" />
+            <span className="sidebarFriendName">
+              Jane doe
+            </span>
+          </li>
+        </ul>
+      </div>
     </SideBarContainer>
   )
 }
--- FrontEnd/src/components/SideBar/styles.ts
+++ FrontEnd/src/components/SideBar/styles.ts
@@ -2,4 +2,64 @@ import styled from "styled-components";
 
 export const SideBarContainer = styled.div`
   flex: 3;
+  height: calc(100vh - 3.125rem);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #f1f1f1;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: rgb(179, 179, 179);
+  }
+
+  .sidebarWrapper {
+    padding: 1.25rem;
+
+    .sidebarList {
+      .sidebarListItem {
+        display: flex;
+        align-items: center;
+        margin-bottom: 1.25rem;
+
+        .sidebarIcon {
+          margin-right: 0.93rem;
+        }
+
+        .sidebarListItemText {}
+      }
+    }
+
+    .sidebarButton {
+      width: 9.375rem;
+      border: none;
+      padding: 0.625rem;
+      border-radius: 0.3rem;
+      font-weight: 500;
+    }
+
+    .sidebarHr {
+      margin: 1.25rem 0;
+    }
+
+    .sidebarFriendList {
+      .sidebarFriend {
+        display: flex;
+        align-items: center;
+        margin-bottom: 0.93rem; 
+
+        .sidebarFriendImg {
+          width: 2rem;
+          height: 2rem;
+          border-radius: 50%;
+          object-fit: cover;
+          margin-right: 0.625rem;
+        }
+
+        .sidebarFriendName {}
+      }
+    }
+  }
 `;
